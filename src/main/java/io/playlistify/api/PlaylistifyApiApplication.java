7ce761f5e24a05cb869ecea6f88f7f050aeebd57
Merijn Schreijen <merijn.schreijen@gmail.com>
2023-09-08 11:28:02 +0200

    initial project creation

---
+++ src/main/java/io/playlistify/api/PlaylistifyApiApplication.java
@@ -0,0 +1,13 @@
+package io.playlistify.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistifyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/playlistify/api/PlaylistifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.playlistify.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
